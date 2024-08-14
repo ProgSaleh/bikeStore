044521a54f635461a6ef6d26c31394cc12f62cb6
Progsaleh <progsaleh@hotmail.com>
2024-08-14 14:33:44 +0300

    remove comments

--- Entities/Production/Product.cs
+++ Entities/Production/Product.cs
@@ -3,37 +3,31 @@ namespace bikeStore.Entities.Production
   [Table("products", Schema = "production")]
   public class Product
   {
-	// product_id INT IDENTITY (1, 1) PRIMARY KEY,
   [Key]
   [Column("product_id")]
   public int Id { get; set; }
 
 
-	// product_name VARCHAR (255) NOT NULL,
   [Column("product_name")]
   [MaxLength(255)]
   [Required]
   public required string ProductName { get; set; }
 
-	// brand_id INT NOT NULL,
   [Column("brand_id")]
   [Required]
   // [ForeignKey]
   public int BrandId { get; set; }
 
-	// category_id INT NOT NULL,
   [Column("category_id")]
   [Required]
   public required int CategoryId { get; set; }
 
   
-	// model_year SMALLINT NOT NULL,
   [Column("model_year")]
   [Required]
   public required short ModelYear { get; set; }
 
 
-	// list_price DECIMAL (10, 2) NOT NULL,
   [Column("list_price")]
   [Required]
   [Precision(10, 2)]
--- Entities/Production/Stock.cs
+++ Entities/Production/Stock.cs
@@ -3,15 +3,12 @@ namespace bikeStore.Entities.Production
   [Table("stocks", Schema = "production")]
   public class Stock
   {
-	// store_id INT,
   [Column("store_id")]
   public int StoreId { get; set; }
 
-	// product_id INT,
   [Column("product_id")]
   public int ProductId { get; set; }
 
-	// quantity INT,
   [Column("quantity")]
   public int Quantity { get; set; }
 
